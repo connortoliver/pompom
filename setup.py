07478b5b4fa76ab72e254160528cda7ee02dcdb4
connortoliver <connorstoliver@gmail.com>
2019-02-01 14:27:23 -0600

    Updated required modules

--- setup.py
+++ setup.py
@@ -3,16 +3,18 @@ import setuptools
 setuptools.setup(
     name='pompom',
     version='0.1.0',
-    description='A simple pomodoro timer and time tracker',
+    description='A CLI pomodoro timer and time tracker',
     url='https://github.com/connortoliver/pompom',
     packages=setuptools.find_packages(),
     install_requires=[
-        'Click>=7.0',
+        'Click',
+        'playsound',
     ],
-    entry_points='''
-        [console_scripts]
-        pompom=pompom.__main__:main
-    ''',
+    entry_points={
+        'console_scripts': [
+            'pompom=pompom-cli.__main__:main'
+        ],
+    },
     classifiers=[
         'Programming Language :: Python :: 3',
     ],

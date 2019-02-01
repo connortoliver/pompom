5d97495c3d39a6607e976361bcb6031df715fbf5
connortoliver <connorstoliver@gmail.com>
2019-01-31 21:21:02 -0600

    Updated file structure and file names.

--- setup.py
+++ setup.py
@@ -2,13 +2,13 @@ from setuptools import setup
 
 setup(
     name='pompom',
-    version='0.1',
-    py_modules=['pompom'],
+    version='0.1.0',
+    packages=['pompom'],
     install_requires=[
         'Click',
     ],
     entry_points='''
         [console_scripts]
-        pompom=pompom:timer
+        pompom=pompom.__main__:main
     ''',
 )
